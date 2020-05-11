51c3f5c755df6a57230447efbcd968fb96dd0fd8
Carly L <carlylynnla@gmail.com>
2020-05-11 22:49:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   string_to_array = string.split(" ")
-  strings_in_array_reversed = string_to_array.collect do |element|
+  string_to_array.collect do |element|
     element.reverse
   end
-  strings_in_array_reversed.join(" ")
 end
